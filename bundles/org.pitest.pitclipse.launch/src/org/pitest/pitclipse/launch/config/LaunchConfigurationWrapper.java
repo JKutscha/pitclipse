f30ba3a2af91a0178e0f9d2a1b075e09ea6ff162
Jonas Kutscha <jonas.kutscha@advantest.com>
2021-08-04 11:25:31 +0200

    Removed isBlank(), because java 11

--- bundles/org.pitest.pitclipse.launch/src/org/pitest/pitclipse/launch/config/LaunchConfigurationWrapper.java
+++ bundles/org.pitest.pitclipse.launch/src/org/pitest/pitclipse/launch/config/LaunchConfigurationWrapper.java
@@ -266,7 +266,7 @@ public class LaunchConfigurationWrapper {
     private List<String> getTargetClasses() throws CoreException {
         ImmutableList.Builder<String> results = ImmutableList.builder();
         final String targetClasses = launchConfig.getAttribute(ATTR_TARGET_CLASSES, "");
-        return targetClasses.isBlank() ? null
+        return targetClasses.equals("") ? null
                 : results.addAll(Splitter.on(',').trimResults().omitEmptyStrings().split(targetClasses)).build();
     }
 
